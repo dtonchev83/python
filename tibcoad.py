bf04bfd9e16f6cd6a660fe23be9e55e42dfb4e42
dtonchev83 <40528390+dtonchev83@users.noreply.github.com>
2020-01-07 14:13:33 +0200

    Add files via upload

---
+++ tibcoad.py
@@ -0,0 +1,7 @@
+'''
+1 download ear files from svn reveision id
+2 check name for every ear file in svn revision
+3 map to the application on the environment
+4 export configuration xml from the ear
+5 prepare configuration xml files for every environment
+'''
